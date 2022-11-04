672845f5541102ac1bbee4ded07e37eb06942d5b
I10kram <114987248+I10kram@users.noreply.github.com>
2022-11-04 20:04:26 +0100

    Update armes.py

--- armes.py
+++ armes.py
@@ -41,3 +41,29 @@ class torpilles(Armes):
             super().fire_at(x,y,z)
             if not z<=0:
                 raise OutOfRangeError
+                
+"""test unitaire"""
+if __name__ == "__main__":
+    antisurfaces= antisurfaces()
+    antiair = antiair()
+    torpilles= torpilles()
+    antisurfaces.fire_at(0,0,0)
+    assert antisurfaces.ammunations == 35
+    try:
+        antisurfaces.fire_at(2,0,1)
+    except:
+        assert antisurfaces.ammunations == 34
+    antiair.fire_at(1,3,5)
+    assert antiair.ammunations == 49
+    try:
+        antiair.fire_at(0,0,0)
+    except:
+        assert LMAR.ammunations == 48
+    torpilles.fire_at(0,0,0)
+    assert torpilles.ammunations == 14
+    torpilles.fire_at(0,0,-1)
+    assert torpilles.ammunations == 13
+    try:
+        torpilles.fire_at(0,0,1)
+    except:
+        assert torpilles.ammunations == 12
